ee4780bc1f558df1538882d7c9dcf2651ec95b2e
artemitchggwp <milangomola.official@gmail.com>
2025-01-21 09:25:45 +0100

    Yessssss

---
+++ 4.Basicc copy 2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+    cout << "Hey" << endl;
+    
+    cout << "Hey" << endl;
+    
+    cout << "Hey" << endl;
+    return 0;
+}
