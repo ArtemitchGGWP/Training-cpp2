3b20b984b70352593405a4777a8e96b6720aa521
Artemitch <MilanGomola.official@gmail.com>
2026-05-01 05:58:17 +0300

    Almost all Operators - challenge

---
+++ 020.LogicalOperators/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num{};
+    const int lower{10};
+    const int upper{20};
+
+    cout << boolalpha;
+
+    cout << "Enter an interger - the bounds are " << lower << " and " << upper << " : ";
+    cin >> num;
+    
+    bool within_bounds{false};
+    within_bounds = (num >= lower && num <= upper);
+    cout << num << " is between " << lower << " and " << upper << " : " << within_bounds << endl;
+
+    bool outside_bounds{false};
+    outside_bounds = (num < lower || num > upper);
+    cout << num << " is outside " << lower << " and " << upper << " : " << outside_bounds << endl;
+
+    bool on_bounds{false};
+    on_bounds = (num == lower || num == upper);
+    cout << num << " is on bounds of " << lower << " or " << upper << " : " << on_bounds << endl;
+
+    bool wear_coat{false};
+    double temperature{};
+    int wind_speed{};
+
+    const double temperature_for_coat {45};
+    const int wind_speed_for_coat{25};
+    
+    cout << "\nEnter the current temperature in (F): ";
+    cin >> temperature;
+    cout << "Enter windspeed in (mph): ";
+    cin >> wind_speed;
+
+    wear_coat = (temperature < temperature_for_coat || wind_speed > wind_speed_for_coat);
+    cout << "Do you need to wear a coat using OR? " << wear_coat << endl;
+
+    wear_coat = (temperature < temperature_for_coat && wind_speed > wind_speed_for_coat);
+    cout << "Do you need to wear a coat using AND? " << wear_coat;
+
+
+
+    return 0;
+}
